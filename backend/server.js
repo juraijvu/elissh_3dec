70c70490a87ba83d2c7feca196f325685d164981
juraijvu <juraijvuit@gmai.com>
2025-12-03 15:59:57 +0400

    server changes

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use(helmet({
   }
 }));
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5000'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5000', 'http://elissh.com', 'https://elissh.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Cache-Control']
